18ea55d1d81ba7a6c612d442a631880c0970c848
SolarLight <SolarLightTwo@gmail.com>
2026-07-19 19:29:53 +0100

    update viewmodel fov dev note

--- src/game/client/view.cpp
+++ src/game/client/view.cpp
@@ -106,8 +106,10 @@ static ConVar v_centermove( "v_centermove", "0.15");
 static ConVar v_centerspeed( "v_centerspeed","500" );
 
 #ifdef TF_CLIENT_DLL
-// 54 degrees approximates a 35mm camera - we determined that this makes the viewmodels
+// TF2 DEV NOTE: 54 degrees approximates a 35mm camera - we determined that this makes the viewmodels
 // and motions look the most natural.
+
+//P4SS: Raised to 70. This is the highest FOV before visual issues become way too apparent, and it better matches the raised FOV of the world (to 90) compared to TF2's default fov_desired of 70.
 ConVar v_viewmodel_fov( "viewmodel_fov", "70", FCVAR_ARCHIVE, "Sets the field-of-view for the viewmodel.", true, 0.1, true, 179.9, true, 54, true, 70, NULL );
 #else
 ConVar v_viewmodel_fov( "viewmodel_fov", "70", FCVAR_CHEAT, "Sets the field-of-view for the viewmodel.", true, 0.1, true, 179.9 );
